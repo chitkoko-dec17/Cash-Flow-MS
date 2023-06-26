12ee7a7de7082e19091d6aa1c054ac6886f9d837
nyanlinn <nyan.linn.tun@zabai.no>
2023-06-26 21:45:23 +0630

    copy theme to project and link to login page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $project_title = '| Cash Flow Management System';
+        View::share('title', $project_title);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+@include_once('admin_web.php');
+
 Route::get('/', function () {
-    return view('welcome');
+   return view('welcome');
 });
+
+Route::get('/login', function () {
+    return redirect()->route('login');
+});
+
+// login route setup
+Route::prefix('login')->group(function () {
+    Route::view('/', 'admin.authentication.login')->name('login');
+    Route::view('default', 'admin.authentication.login')->name('login.index');
+});
+
+
+Route::get('/test-db-connection', function () {
+    try {
+        DB::connection()->getPdo();
+       // DB::select('select * from users where active = ?', [1])
+
+        return 'Connected to the database!';
+    } catch (\Exception $e) {
+        return 'Failed to connect to the database: ' . $e->getMessage();
+    }
+});
+
+Route::view('sample-page', 'admin.pages.sample-page')->name('sample-page');
+
+Route::prefix('dashboard')->group(function () {
+    Route::view('/', 'admin.dashboard.default')->name('index');
+    Route::view('default', 'admin.dashboard.default')->name('dashboard.index');
+});
+
+Route::view('default-layout', 'multiple.default-layout')->name('default-layout');
+Route::view('compact-layout', 'multiple.compact-layout')->name('compact-layout');
+Route::view('modern-layout', 'multiple.modern-layout')->name('modern-layout');
