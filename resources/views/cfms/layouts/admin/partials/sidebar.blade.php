ea90a66b2cdf3e19ee7cfaeee90bcfd5ccb8bd6f
waiphyoaung.dev <waiphyoaung.dev@gmail.com>
2023-09-06 00:44:36 +0630

    logo adjustment for three different roles

--- resources/views/cfms/layouts/admin/partials/sidebar.blade.php
+++ resources/views/cfms/layouts/admin/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <header class="main-nav">
     <div class="sidebar-user text-center">
-        <a class="setting-primary" href="{{ route('user.profile') }}"><i data-feather="settings"></i></a><img class="img-90 rounded-circle" src="@if(Auth::user()->user_role == "Admin") {{asset('assets/images/logo/profile.png')}} @else {{asset('storage/'.Auth::user()->businessunit->bu_image)}} @endif" alt="" />
+        <a class="setting-primary" href="{{ route('user.profile') }}"><i data-feather="settings"></i></a><img class="img-90 rounded-circle" src="@if(Auth::user()->user_role == "Admin") {{asset('assets/images/logo/profile.png')}} @elseif(Auth::user()->user_role == "Manager") {{asset('storage/'.Auth::user()->businessunit->bu_image)}} @else {{asset('storage/'.Auth::user()->branchUser->branch->businessUnit->bu_image)}} @endif" alt="" />
         {{-- <div class="badge-bottom"><span class="badge badge-primary">New</span></div> --}}
         <a href="javascript:void(0)"> <h6 class="mt-3 f-14 f-w-600">{{ Auth::user()->name }}</h6></a>
         <p class="mb-0 font-roboto">{{ Auth::user()->user_role }}</p>
